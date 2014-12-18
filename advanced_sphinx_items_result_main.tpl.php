176206013c2c68612954965899834be839e335f7
Sergey Pavlenko <sergeypavlenko@1343804.no-reply.drupal.org>
2014-12-18 02:33:09 +0200

    The control statement should use the ":" alternative syntax instead of curly braces in template files.

--- advanced_sphinx_items_result_main.tpl.php
+++ advanced_sphinx_items_result_main.tpl.php
@@ -17,24 +17,24 @@
   <h3 class="title-result"><span
       class="number-result"><?php print $result['number']; ?>
       .</span> <?php print $result['title']; ?></h3>
-  <?php if (isset($result['excerpts'])) { ?>
+  <?php if (isset($result['excerpts'])): ?>
     <div class="content-result">
       <?php print $result['excerpts']; ?>
     </div>
-  <?php } ?>
+  <?php endif; ?>
 
   <div class="info-result">
-    <?php if (isset($result['date'])) { ?>
+    <?php if (isset($result['date'])): ?>
       <span class="date-result"><?php print $result['date']; ?>,</span>
-    <?php } ?>
+    <?php endif; ?>
 
-    <?php if (isset($result['username'])) { ?>
+    <?php if (isset($result['username'])): ?>
       <span class="autor-result"><?php print $result['username']; ?></span>
-    <?php } ?>
+    <?php endif; ?>
 
-    <?php if (isset($result['tags'])) { ?>
+    <?php if (isset($result['tags'])): ?>
       <span
         class="tax-result"><?php print t('Tags') . ': ' . $result['tags']; ?></span>
-    <?php } ?>
+    <?php endif; ?>
   </div>
 </li>
